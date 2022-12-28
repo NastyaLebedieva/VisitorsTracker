3c11ebb044a88f9a27abb2250a28820a19811fd4
AnastasiiaLebedieva <95622147+AnastasiiaLebedieva@users.noreply.github.com>
2022-12-28 22:49:10 +0200

    updated Message model

--- VisitorsTracker.Common/Message.cs
+++ VisitorsTracker.Common/Message.cs
@@ -1,18 +1,21 @@
 ﻿using System.Globalization;
 
-namespace Common
+namespace VisitorsTracker.Common
 {
     public class Message
     {
-        public string? Referrer { get; set; }
+        public string Referrer { get; set; }
 
-        public string? UserAgent { get; set; }
+        public string UserAgent { get; set; }
 
         public string ClientIp { get; set; }
 
         public DateTime Date { get; set; }
 
         public override string ToString() =>
-            $"{Date.ToString("o", CultureInfo.InvariantCulture)}|{Referrer ?? "null"}|{UserAgent ?? "null"}|{ClientIp}";
+            $"{Date.ToString("o", CultureInfo.InvariantCulture)}|" +
+            $"{(string.IsNullOrEmpty(Referrer) ? "null" : Referrer)}|" +
+            $"{(string.IsNullOrEmpty(UserAgent) ? "null" : UserAgent)}|" +
+            $"{ClientIp}";
     }
 }
